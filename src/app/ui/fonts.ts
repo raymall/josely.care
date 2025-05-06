0b7c0cd52c3b1bf1836bdde09e9cc5dd0b9dd645
Raymall Perez <raymall.perez@gmail.com>
2025-05-06 14:35:04 -0400

    Add colors and global styles

--- src/app/ui/fonts.ts
+++ src/app/ui/fonts.ts
@@ -1,9 +1,10 @@
 import {
   Work_Sans,
   Fira_Sans,
-  // Libre_Franklin,
   Chivo,
-  // Rubik
+  Nunito,
+  Encode_Sans,
+  IBM_Plex_Serif
 } from 'next/font/google'
 
 export const work_sans = Work_Sans({
@@ -20,13 +21,6 @@ export const fira_sans = Fira_Sans({
   variable: '--font-fira-sans',
 })
 
-// export const libre_franklin = Libre_Franklin({
-//   subsets: ['latin'],
-//   weight: ['400', '500', '700', '900'],
-//   style: ['normal', 'italic'],
-//   variable: '--font-libre-franklin',
-// })
-
 export const chivo = Chivo({
   subsets: ['latin'],
   weight: ['400', '500', '700', '900'],
@@ -34,9 +28,23 @@ export const chivo = Chivo({
   variable: '--font-chivo',
 })
 
-// export const rubik = Rubik({
-//   subsets: ['latin'],
-//   weight: ['400', '500', '700', '900'],
-//   style: ['normal', 'italic'],
-//   variable: '--font-rubik',
-// })
+export const nunito = Nunito({
+  subsets: ['latin'],
+  weight: ['400', '500', '700', '900'],
+  style: ['normal', 'italic'],
+  variable: '--font-nunito',
+})
+
+export const encode_sans = Encode_Sans({
+  subsets: ['latin'],
+  weight: ['400', '500', '600', '700', '800', '900'],
+  style: ['normal'],
+  variable: '--font-encode-sans',
+})
+
+export const ibm_plex_serif = IBM_Plex_Serif({
+  subsets: ['latin'],
+  weight: ['400', '500', '600', '700'],
+  style: ['normal'],
+  variable: '--font-ibm-plex-serif',
+})
