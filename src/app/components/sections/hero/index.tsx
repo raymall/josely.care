d9ab7c3d19110034c549bed7d48f77e81eec837b
Raymall Perez <raymall.perez@gmail.com>
2025-05-06 14:37:34 -0400

    Add Hero section

---
+++ src/app/components/sections/hero/index.tsx
@@ -0,0 +1,33 @@
+import Link from 'next/link'
+import { WhatsappIcon } from '@/app/ui/icons'
+import styles from './styles.module.scss'
+import ContactSection from '@/app/components/sections/contact'
+import DividerSection from '../divider'
+
+export default function HeroSection() {
+  return (
+    <section className={styles.Hero}>
+      <div className={styles.Hero_content}>
+        <div className={styles.Hero_info}>
+          <h1 className={styles.Hero_title}>
+            <span>Los seguros </span>
+            <span className={styles.Hero_title__highlight}>no </span> <br/>
+            <span>son complicados. </span>
+          </h1>
+          <Link
+            className={styles.Hero_button}
+            href={`https://wa.me/18296452301?text=${encodeURIComponent('Hola Josely, necesito información sobre seguros.')}`}
+          >
+            Quiero saber más
+            <WhatsappIcon className={styles.Hero_button_icon} />
+          </Link>
+        </div>
+      </div>
+
+      <DividerSection />
+
+      <ContactSection />
+        
+    </section>
+  )
+}
